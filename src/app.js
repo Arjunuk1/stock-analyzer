a7f9ee6e99356df80b900c8b893bccf36a70018f
Arjun Sharma <arjun0941.becse24@chitkara.edu.in>
2026-05-27 13:24:03 +0530

    refactor: prefix API routes with /api and remove static frontend serving

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const session = require('express-session');
 const passport = require('./config/passport');
 const connectDB = require('./config/db');
+const path = require('path');
 
 dotenv.config();
 connectDB();
@@ -22,17 +22,18 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// Provide access to uploaded profile pictures
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 const authRoutes = require('./routes/authRoutes');
 const stockRoutes = require('./routes/stockRoutes');
 
-app.use('/', authRoutes);
-app.use('/', stockRoutes);
+app.use('/api', authRoutes);
+app.use('/api', stockRoutes);
+
+app.get('/', (req, res) => {
+  res.json({ message: "Welcome to Stock Analyzer API! The frontend is now completely separated." });
+});
 
 module.exports = app;
