2928144e1998876efa0dd14fc70264310c734f2e
Arjun Sharma <arjun0941.becse24@chitkara.edu.in>
2026-05-27 13:25:22 +0530

    feat(user): add bcrypt password hashing and Google user management methods

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -8,5 +9,53 @@ const userSchema = new mongoose.Schema({
   profilePic: { type: String, default: '' },
   googleId: { type: String, default: null }
 });
+
+// Pre-save hook to hash password
+userSchema.pre('save', async function() {
+  if (!this.isModified('password') || !this.password) {
+    return;
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  if (!this.password || !candidatePassword) return false;
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+// Static method to find or create google user
+userSchema.statics.findOrCreateGoogleUser = async function(profile) {
+  const { name, email, googleId, profilePic } = profile;
+  let user = await this.findOne({ email });
+
+  if (!user) {
+    user = await this.create({
+      name: name || 'Google User',
+      email,
+      password: null,
+      authProvider: 'google',
+      googleId: googleId || null,
+      profilePic: profilePic || ''
+    });
+  } else {
+    let changed = false;
+    if (!user.authProvider || user.authProvider === 'local') {
+      user.authProvider = 'google';
+      changed = true;
+    }
+    if (!user.googleId) {
+      user.googleId = googleId || null;
+      changed = true;
+    }
+    if (!user.profilePic && profilePic) {
+      user.profilePic = profilePic;
+      changed = true;
+    }
+    if (changed) await user.save();
+  }
+  return user;
+};
 
 module.exports = mongoose.model('User', userSchema);
